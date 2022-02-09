9e214c640a3939f77330dc1c78aeca7e04d5a4d9
lukita <lucasgo1099@gmail.com>
2022-02-08 21:18:40 -0300

    correcting and creating the image list

---
+++ lib/model/Currencyclass.dart
@@ -0,0 +1,11 @@
+class Currency {
+  String label;
+  String icon;
+  String sigla;
+  double price;
+  Currency(
+      {required this.label,
+      required this.icon,
+      required this.price,
+      required this.sigla});
+}
