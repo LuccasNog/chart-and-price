1a13b32d33054512427d64eb0033eea9183cd570
lukita <lucasgo1099@gmail.com>
2022-02-08 22:54:56 -0300

    correcting price

--- lib/controller/List_CurrencyController.dart
+++ lib/controller/List_CurrencyController.dart
@@ -7,47 +7,47 @@ class CurrencyController {
     Currency(
         label: 'Bitcoin',
         icon: 'images/bitcoin.png',
-        price: 250.00,
+        price: 231.858,
         sigla: 'BTC'),
     Currency(
         label: 'Ethereum',
         icon: 'images/ethereum.png',
-        price: 250.00,
+        price: 16.407,
         sigla: 'ETH'),
     Currency(
         label: 'Litecoin',
         icon: 'images/litecoin_icon.png',
-        price: 250.00,
+        price: 707.00,
         sigla: 'LTC'),
     Currency(
         label: 'Dogecoin',
         icon: 'images/doge.png',
-        price: 250.00,
+        price: 707.00,
         sigla: 'DOGE'),
     Currency(
         label: 'Monero',
         icon: 'images/monero.png',
-        price: 250.00,
+        price: 940.00,
         sigla: 'XRM'),
     Currency(
         label: 'Vertcoin',
         icon: 'images/vertcoin.png',
-        price: 250.00,
+        price: 2,
         sigla: 'VERT'),
     Currency(
         label: 'Z-cash',
         icon: 'images/z-cash.png',
-        price: 250.00,
+        price: 698.00,
         sigla: 'ZCASH'),
     Currency(
         label: 'Cardano',
         icon: 'images/ada_cardano.png',
-        price: 250.00,
+        price: 6.21,
         sigla: 'ADA'),
     Currency(
         label: 'Neo',
         icon: 'images/neo-cripto.png',
-        price: 250.00,
+        price: 121.00,
         sigla: 'NEO'),
   ];
 }
