f17fe24e0fc6637ebc62705db49a5d12781e4b4c
lukita <lucasgo1099@gmail.com>
2022-02-15 22:47:40 -0300

    resolved part in connection api

--- lib/controller/List_CurrencyController.dart
+++ lib/controller/List_CurrencyController.dart
@@ -13,13 +13,33 @@ class CurrencyController {
   //retornar os dados da moeda
 //  List<Currency> get table => tabela;
 
-  Future<List> SearchMoedas() async {
+  Future<List<Map<String, dynamic>>> searchCurrencys() async {
+    var andress =
+        Uri.parse('http://api.coinbase.com/v2/assets/search?base=brl');
+    http.Response response = await http.get(andress);
+    if (response.statusCode != 200) {
+      print('error');
+    } else {
+      return List<Map<String, dynamic>>.from(
+          json.decode(response.body)['data']);
+      print(utf8.decode(response.data));
+    }
+  }
+}
+
+/*
+static Future<List> SearchMoedas() async {
     var url = Uri.parse('http://api.coinbase.com/v2/assets/search?base=brl');
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      return jsonDecode(utf8.decode(response.bodyBytes));
+      // return jsonDecode(utf8.decode(response.body));
+      //  print(response.body);
+      tabela = json.decode(response.body);
+      print(tabela);
+      // return json.decode(response.body);
+      return jsonDecode(response.body);
     } else {
       throw Exception('Erro ao acessar API');
     }
   }
-}
+ */ 
