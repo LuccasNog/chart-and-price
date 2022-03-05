aa2b016a42c383b4b567fe3452e9d594535f3422
lucasgo1099@gmail.com <you@example.com>
2022-03-05 14:42:44 -0300

    add page graphic

---
+++ lib/pages/listcurrency.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListCurrency extends StatefulWidget {
+  const ListCurrency({Key? key}) : super(key: key);
+
+  @override
+  State<ListCurrency> createState() => _ListCurrencyState();
+}
+
+class _ListCurrencyState extends State<ListCurrency> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
