6ab94ad14b7ba8e34cdd4292e10fb7133ad9fa57
lucasgo1099@gmail.com <you@example.com>
2022-03-13 14:02:58 -0300

    correction login

--- lib/pages/initialroute.dart
+++ lib/pages/initialroute.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:btc_exchange/pages/listcurrency.dart';
 import 'package:flutter/material.dart';
 
 import 'graphic.dart';
@@ -14,22 +15,66 @@ class InitialRoute extends StatefulWidget {
 class _InitialRouteState extends State<InitialRoute> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Safeway Crypto'),
+              backgroundColor: Colors.black,
+            ),
+            body: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Divider(
+                    height: 60,
+                    color: Colors.transparent,
+                  ),
+                  Image.network(
+                    'https://cryptologos.cc/logos/bitsend-bsd-logo.png?v=022',
+                    width: 190,
+                  ),
+                  TextButton.icon(
+                    icon: Icon(Icons.abc),
+                    label: const Text('Entrar',
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 20,
+                        )),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ListCurrency()),
+                      );
+                    },
+                  )
+                ],
+              ),
+            )));
+  }
+}
+
+
+/*
+Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Image.network(
               'https://cryptologos.cc/logos/bitsend-bsd-logo.png?v=022',
-              height: 190,
+              height: 20,
             ),
+            /*
             const Divider(
               color: Colors.transparent,
             ),
             const Divider(
               color: Colors.transparent,
             ),
+            */
             TextButton(
               style: ButtonStyle(
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
@@ -37,18 +82,16 @@ class _InitialRouteState extends State<InitialRoute> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => const GraphicCurrency()),
+                  MaterialPageRoute(builder: (context) => const ListCurrency()),
                 );
               },
               child: const Text(
                 'Entrar',
                 overflow: TextOverflow.fade,
+                style: TextStyle(fontWeight: FontWeight.bold, height: 45),
               ),
             )
           ],
         ),
       ),
-    );
-  }
-}
+*/
